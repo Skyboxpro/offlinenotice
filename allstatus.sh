a128f81ec2fff2ef7115209a8ccb7b16f955c862
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-01-24 22:32:02 +1100

    Create allstatus.sh

---
+++ allstatus.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if skywire node process is running
+# -x flag only match processes whose name (or command line if -f is
+# specified) exactly match the pattern.
+
+
+./status.sh
+
+iplist="/home/pi/ips.txt"
+while IFS= read -r ip
+do
+        ssh $ip bash -s <<-EOF
+        ./test.sh
+EOF
+
+done < "$iplist"
