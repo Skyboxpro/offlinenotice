0d6105a7aa618ce0a2c2bbd0b281eb2ecffe6f81
Skyboxpro <46995474+Skyboxpro@users.noreply.github.com>
2019-01-24 23:20:55 +1100

    Update initialise.sh

--- initialise.sh
+++ initialise.sh
@@ -30,3 +30,6 @@ done < "$iplist"
 
 #Install Node-Red
 bash <(curl -sL https://raw.githubusercontent.com/node-red/raspbian-deb-package/master/resources/update-nodejs-and-nodered)
+
+sudo systemctl enable nodered.service
+sudo reboot
